a43634954e8e1e0e6cdad88e71f03330cd965900
atopx <3940422@qq.com>
2023-11-03 14:29:08 +0800

    optimize winner detect

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,10 @@ use std::time::{Duration, Instant};
 use self::state::{MoveState, Status};
 
 pub mod book;
+pub mod position;
 pub mod pregen;
 pub mod state;
 pub mod util;
-pub mod position;
 
 #[derive(Clone, Copy, Default)]
 pub struct Hash {
@@ -828,9 +828,13 @@ impl Engine {
         true
     }
 
-    pub fn winner(&mut self) -> isize {
+    pub fn winner(&mut self) -> Option<pregen::Winner> {
         if self.has_mate() {
-            return 1 - self.sd_player;
+            return match 1 - self.sd_player {
+                0 => Some(pregen::Winner::White),
+                1 => Some(pregen::Winner::Black),
+                _ => Some(pregen::Winner::Tie),
+            };
         };
         let pc = pregen::PIECE_KING + util::side_tag(self.sd_player);
         let mut mate = 0;
@@ -841,15 +845,23 @@ impl Engine {
             }
         }
         if mate == 0 {
-            return 1 - self.sd_player;
+            return match 1 - self.sd_player {
+                0 => Some(pregen::Winner::White),
+                1 => Some(pregen::Winner::Black),
+                _ => Some(pregen::Winner::Tie),
+            };
         }
         let mut vl_rep = self.rep_status(3);
         if vl_rep > 0 {
             vl_rep = self.rep_value(vl_rep);
             if -pregen::WIN_VALUE < vl_rep && vl_rep < pregen::WIN_VALUE as isize {
-                return 2;
+                return Some(pregen::Winner::Tie);
             }
-            return self.sd_player;
+            return match self.sd_player {
+                0 => Some(pregen::Winner::White),
+                1 => Some(pregen::Winner::Black),
+                _ => Some(pregen::Winner::Tie),
+            };
         }
         let mut has_material = false;
         for i in 0..self.squares.len() {
@@ -859,9 +871,9 @@ impl Engine {
             }
         }
         if !has_material {
-            return 2;
+            return Some(pregen::Winner::Tie);
         }
-        0
+        None
     }
 
     pub fn new_state(&mut self, hash: isize) -> MoveState {
@@ -1353,7 +1365,7 @@ mod tests {
         let mv = engine.search_main(64, 1000);
         assert_eq!(mv, 22326);
     }
-    
+
     #[test]
     fn test_engine_22985() {
         let fen: &str = "4kab2/4a4/8b/9/9/9/9/9/9/4K1R2 w - - 0 1";
